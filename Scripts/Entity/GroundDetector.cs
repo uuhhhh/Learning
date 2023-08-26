00266f390cf1f0a8253fc3e78164046a038e2c61
immen <chazi@uw.edu>
2023-08-26 00:46:56 -0700

    removed prints

--- Scripts/Entity/GroundDetector.cs
+++ Scripts/Entity/GroundDetector.cs
@@ -4,15 +4,6 @@ using Learning.Scripts.Environment;
 namespace Learning.Scripts.Entity; 
 
 public partial class GroundDetector : EnvObjectDetector {
-    public override void _Ready() {
-        base._Ready();
-
-        EnvObjectEnteredArea += eo => GD.Print($"ground entered: {eo}");
-        EnvObjectExitedArea += eo => GD.Print($"ground exited: {eo}");
-        NewHighestPriorityEnvObject += eo => GD.Print($"new highest prio: {eo}");
-        ZeroToOneEnvObjects += () => GD.Print("zero to one grounds");
-        ZeroEnvObjects += () => GD.Print("zero grounds");
-    }
     protected override int GetPriorityOf(EnvObject envObject) {
         return envObject.GroundPriority;
     }
