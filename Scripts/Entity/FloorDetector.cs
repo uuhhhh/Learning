5b65451047bcd73452737ef393b443c4fa8c22eb
immen <chazi@uw.edu>
2023-08-26 04:43:32 -0700

    GroundDetector rename

--- Scripts/Entity/FloorDetector.cs
+++ Scripts/Entity/FloorDetector.cs
@@ -3,7 +3,7 @@ using Learning.Scripts.Environment;
 
 namespace Learning.Scripts.Entity; 
 
-public partial class GroundDetector : EnvObjectDetector {
+public partial class FloorDetector : EnvObjectDetector {
     protected override int GetPriorityOf(EnvObject envObject) {
         return envObject.GroundPriority;
     }
