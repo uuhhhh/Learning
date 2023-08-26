cce8b1ec549fd2559c9aca351f933564e4d66c36
immen <chazi@uw.edu>
2023-08-26 00:46:39 -0700

    Added wall and ground detectors

---
+++ Scripts/Entity/WallDetector.cs
@@ -0,0 +1,20 @@
+﻿using Godot;
+using Learning.Scripts.Environment;
+
+namespace Learning.Scripts.Entity; 
+
+public partial class WallDetector : EnvObjectDetector {
+    public override void _Ready() {
+        base._Ready();
+
+        EnvObjectEnteredArea += eo => GD.Print($"wall entered: {eo}");
+        EnvObjectExitedArea += eo => GD.Print($"wall exited: {eo}");
+        NewHighestPriorityEnvObject += eo => GD.Print($"new highest prio: {eo}");
+        ZeroToOneEnvObjects += () => GD.Print("zero to one walls");
+        ZeroEnvObjects += () => GD.Print("zero walls");
+    }
+    
+    protected override int GetPriorityOf(EnvObject envObject) {
+        return envObject.WallPriority;
+    }
+}
---
+++ Scripts/Environment/EnvObject.cs
@@ -0,0 +1,8 @@
+﻿using Godot;
+
+namespace Learning.Scripts.Environment; 
+
+public partial class EnvObject : Node2D {
+    [Export] public int GroundPriority { get; private set; }
+    [Export] public int WallPriority { get; private set; }
+}
