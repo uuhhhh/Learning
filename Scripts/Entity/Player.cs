9bf3c88e9993b9fad29459dacc93f1d001d77528
immen <chazi@uw.edu>
2024-01-01 11:30:25 -0800

    slight restructure of player scene

--- Scripts/Entity/Player.cs
+++ Scripts/Entity/Player.cs
@@ -1,30 +1,28 @@
-﻿using Godot;
+using Godot;
 using Learning.Scripts.Entity.Physics;
 
 namespace Learning.Scripts.Entity; 
 
-public partial class Player : Node2D {
+public partial class Player : KinematicComp {
     public InputComp Input { get; private set; }
     public PlayerVelocityAggregate PlayerController { get; private set; }
-    public KinematicComp PhysicalPlayer { get; private set; }
     public FloorDetector FloorDetector { get; private set; }
     public WallDetector WallDetector { get; private set; }
 
     public override void _Ready() {
+        base._Ready();
+        
         SetChildren();
 
         ConfigureInput();
 
         WallDetector.ZeroToOneEnvObjects += () => PlayerController.CanDoWallBehavior = true;
         WallDetector.ZeroEnvObjects += () => PlayerController.CanDoWallBehavior = false;
-        
-        ProcessPhysicsPriority = PhysicalPlayer.ProcessPhysicsPriority + 1;
     }
 
     private void SetChildren() {
         Input = GetNode<InputComp>(nameof(InputComp));
         PlayerController = GetNode<PlayerVelocityAggregate>(nameof(PlayerVelocityAggregate));
-        PhysicalPlayer = GetNode<KinematicComp>(nameof(VelocityAggregatingKinematicComp));
         FloorDetector = GetNode<FloorDetector>(nameof(FloorDetector));
         WallDetector = GetNode<WallDetector>(nameof(WallDetector));
     }
@@ -39,6 +37,8 @@ public partial class Player : Node2D {
     }
 
     public override void _PhysicsProcess(double delta) {
-        PhysicalPlayer.SetParentPositionToOwn(this);
+        Velocity = PlayerController.Velocity;
+		
+        MoveAndSlideWithStatusChanges();
     }
 }
